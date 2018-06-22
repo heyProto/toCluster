311aa97340a5dab4b9b83c6e0c571744f078ddb5
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2018-06-22 20:29:57 +0530

    Updates SSR code

--- main_ssr.js
+++ main_ssr.js
@@ -4,6 +4,34 @@ import Cluster from './src/js/card.jsx'
 
 global.window = {}
 
+function renderWithMode(mode) {
+    switch (mode) {
+        case "col7":
+            return "x.renderCol7()";
+        case "col4":
+            return "x.renderCol4()";
+        case "col3":
+            return "x.renderCol3()";
+        default:
+            return "x.renderCol4()";
+    }
+}
+
+function getScriptString(mode, dataJSON, selector, site_configs) {
+    return `<script>
+            var x = new ProtoGraph.Card.ComposeCard(),
+                params = {
+                    "selector": document.querySelector('${selector}'),
+                    "isFromSSR": true,
+                    "initialState": ${JSON.stringify(dataJSON)},
+                    "site_configs": ${JSON.stringify(site_configs)}
+                };
+            x.init(params);
+            ${renderWithMode(mode)}
+        </script>
+    `
+}
+
 function render(mode, initialState) {
     let content = renderToString(
         <Cluster
@@ -17,5 +45,6 @@ function render(mode, initialState) {
 }
 
 module.exports = {
-    render: render
+    render: render,
+    getScriptString: getScriptString
 }
